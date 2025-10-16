c3722140d234f80ee0cb889320b50b53eea2ea20
purewave0 <purewave_@outlook.com>
2025-10-16 18:02:18 -0300

    feat: add CLI quizzer

---
+++ kaeru-cli.py
@@ -0,0 +1,155 @@
+"""Quiz adjective and verb conjugations on the command line."""
+
+import json
+import random
+import sys
+from time import sleep
+import readline
+assert readline  # silence linter error
+
+import conjugator
+
+from inflection import (
+    AdjectiveType, AdjectiveInflection,
+    VerbType, VerbInflection
+)
+
+
+def formatted_adjective_question(
+    adjective: str,
+    kana_reading: str | None,
+    type: AdjectiveType,
+    inflection: AdjectiveInflection
+):
+    """Return the given adjetcive + inflection info as a question."""
+    return (
+        f'word: {adjective}' + ('' if not kana_reading else f' ({kana_reading})')
+        + f'\ntype: {type.value}'
+        + f'\nconjugate to: {inflection.formatted()}'
+    )
+
+
+def formatted_verb_question(
+    verb: str,
+    kana_reading: str | None,
+    type: VerbType,
+    inflection: VerbInflection
+):
+    """Return the given verb + inflection info as a question."""
+    return (
+        f'word: {verb}' + ('' if not kana_reading else f' ({kana_reading})')
+        + f'\ntype: {type.value}'
+        + f'\nconjugate to: {inflection.formatted()}'
+    )
+
+
+ATTEMPT_INTERVAL_SECONDS = 1
+
+if __name__ == '__main__':
+    words: list[dict]
+    try:
+        with open('vocab.json') as vocab_file:
+            words = json.load(vocab_file)
+    except FileNotFoundError:
+        print(
+            'error: the vocab.json file does not exist. please run'
+            + ' `python3 make-vocab.py` to build it.',
+            file=sys.stderr
+        )
+        exit(1)
+    except OSError:
+        print(
+            'error: could not open vocab.json.',
+            file=sys.stderr
+        )
+        raise
+    except json.decoder.JSONDecodeError:
+        print(
+            'error: vocab.json is malformed. please rebuild it with'
+            + ' `python3 make-vocab.py`.',
+            file=sys.stderr
+        )
+        exit(2)
+
+    if not words:
+        print(
+            'error: vocab.json is malformed. please rebuild it with'
+            + ' `python3 make-vocab.py`.',
+            file=sys.stderr
+        )
+        exit(2)
+
+    print(f'{len(words)} words loaded.\n')
+
+    while True:
+        random_word: dict = random.choice(words)
+        kana_reading: str | None = random_word['kana']
+        dictionary_form_word: str = random_word['word']
+        type_str: str = random_word['type']
+
+        correctly_conjugated_word = None
+        question = None
+        if type_str.startswith('verb'):
+            try:
+                verb_type = VerbType(type_str)
+            except ValueError:
+                print(
+                    f'error: verb {dictionary_form_word} of illegal type'
+                    + f' "{type_str}"',
+                    file=sys.stderr
+                )
+                exit(4)
+
+            random_inflection = VerbInflection.generate_random()
+            correctly_conjugated_word = conjugator.conjugate_verb(
+                dictionary_form_word, verb_type, random_inflection,
+            )
+            question = formatted_verb_question(
+                dictionary_form_word, kana_reading, verb_type, random_inflection,
+            )
+        elif type_str.startswith('adjective'):
+            try:
+                adjective_type = AdjectiveType(type_str)
+            except ValueError:
+                print(
+                    f'error: adjective {dictionary_form_word} of illegal type'
+                    + f' "{type_str}"',
+                    file=sys.stderr
+                )
+                exit(4)
+            random_inflection = AdjectiveInflection.generate_random(adjective_type)
+            correctly_conjugated_word = conjugator.conjugate_adjective(
+                dictionary_form_word, adjective_type, random_inflection,
+            )
+            question = formatted_adjective_question(
+                dictionary_form_word, kana_reading, adjective_type, random_inflection,
+            )
+        else:
+            print(
+                f'error: word {dictionary_form_word} of illegal type'
+                + f' "{type_str}"',
+                file=sys.stderr
+            )
+            exit(4)
+
+        while True:
+            try:
+                print(question)
+                user_answer = input('your answer: ').strip().lower()
+            except KeyboardInterrupt:
+                print('\nenter "q" or hit CTRL+D to quit.\n')
+                continue
+            except EOFError:
+                print()
+                quit(0)
+
+            if user_answer == 'q':
+                exit(0)
+
+            if user_answer == correctly_conjugated_word:
+                print('correct! next one…\n')
+                sleep(ATTEMPT_INTERVAL_SECONDS)
+                break
+
+            print('wrong answer! try again.\n')
+            sleep(ATTEMPT_INTERVAL_SECONDS)
